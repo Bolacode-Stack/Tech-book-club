ebf5ecd5c05ae6c2afc2500028848b95a484ecec
Mobolaji Ajibola Jr <bolaex@yahoo.com>
2025-03-22 09:38:39 +0100

    Made Changes

--- Dev-Files/tech.js
+++
@@ -1,6 +0,0 @@
-const div = document.createElement('div');
-
-div.style.cssText = ' width: 200px; height: 200px; background-color: orange; border-radius:150px ;'
-// div.innerText = 'I am big and rounded';
-
-// document.body.appendChild(div);
